5414661887dba4e8f16407a530aa7a6aa355cad5
Yurii Palahitskyi <brebston7@gmail.com>
2026-04-07 12:24:06 +0200

    feat: Add JWT authentication and user management API endpoints

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/users/", include("users.api.v1.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token-verify"),
 ]
